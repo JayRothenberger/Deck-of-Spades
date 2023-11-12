a5fd5f6837abfa9dcc229c0a54395c2d4d39f2b8
JayRoth-sch <35668950+JayRothenberger@users.noreply.github.com>
2023-11-11 21:22:40 -0600

    adding remote input for relative mouse movements

---
+++ client/uinput_server.py
@@ -0,0 +1,41 @@
+import socket
+import uinput
+
+if __name__ == "__main__":
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 5005
+
+    sock = socket.socket(socket.AF_INET, # Internet
+                        socket.SOCK_DGRAM) # UDP
+    sock.bind((UDP_IP, UDP_PORT))
+
+    cmd_strings = []
+
+    with open('/src/python-uinput-master/src/ev.py', 'r') as fp:
+        for line in fp.readlines():
+            cmd_strings.append(line.split(' ')[0])
+
+    # tuple([getattr(uinput, cmd) for cmd in cmd_strings])
+    events = (uinput.REL_X, uinput.REL_Y)
+
+    with uinput.Device(events) as device:
+
+        while True:
+            emit = True
+
+            data, addr = sock.recvfrom(256) # buffer size is 256 bytes
+            print(data)
+            cmd, fn, value, syn = data.split(':')
+
+            if cmd == 'emit':
+                emit = True
+
+            fn = getattr(input_device, fn)
+            cmd = getattr(uinput, cmd)
+            value = int(value)
+            syn = bool(syn)
+
+            if emit:
+                fn(cmd, value, syn)
+            else:
+                fn(cmd, syn)
--- vncpython/test.py
+++ vncpython/test.py
@@ -1,15 +1,36 @@
 import asyncio, asyncvnc
 from PIL import Image
 import time
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+SOCK = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+
+def socket_toem(cmd):
+    SOCK.sendto(cmd, (UDP_IP, UDP_PORT))
+
 
 async def run_client():
     async with asyncvnc.connect('localhost', 5901, password='vncpassword') as client:
-        print(client)
+
         # Retrieve pixels as a 3D numpy array
         pixels = await client.screenshot()
         
         # Save as PNG using PIL/pillow
         image = Image.fromarray(pixels)
-        image.save('screenshot.png')
+        image.save('screenshot0.png')
+        client.keyboard.write('111')
+
+        with client.keyboard.hold('Tab'):
+            pixels = await client.screenshot()
+            # Save as PNG using PIL/pillow
+            image = Image.fromarray(pixels)
+            image.save('screenshot1.png')
+
+        socket_toem('REL_X:emit:5:False')
+        socket_toem('REL_Y:emit:5:True')
 
 asyncio.run(run_client())
